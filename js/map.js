3add3a0691368d43f29cabbbce7aa3fa9d408c56
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-07 11:43:23 +0500

    Заведены массивы исходных данных

--- js/map.js
+++ js/map.js
@@ -2,3 +2,13 @@
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
+var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+
+var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+
+var OFFER_CHECKS = ['12:00', '13:00', '14:00'];
+
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg]', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
