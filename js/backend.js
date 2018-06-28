bc3652fa766ccc874e9d38dd34516e3cd4843ef0
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-28 22:33:07 +0500

    Добавление обработки серверных ошибок в backend

--- js/backend.js
+++ js/backend.js
@@ -10,10 +10,27 @@
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+
+        default:
+          error = 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText;
+      }
+      if (error) {
+        onError(error);
       }
     });
     xhr.addEventListener('error', function () {
