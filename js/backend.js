d889ae7a92fc1803cfc56154282247cb4354d485
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-28 12:59:36 +0500

    Наполнение функций в файле backend.js

--- js/backend.js
+++ js/backend.js
@@ -2,4 +2,53 @@
 
 (function () {
 
+  // Функция для загрузки объявлений с сервера
+  window.download = function (onLoad, onError) {
+    var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', LOAD_URL);
+    xhr.send();
+  };
+
+  // Функция для отправки данных на сервер
+  window.save = function (data, onLoad, onError) {
+    var SAVE_URL = 'https://js.dump.academy/keksobooking';
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('POST', SAVE_URL);
+    xhr.send(data);
+  };
 })();
