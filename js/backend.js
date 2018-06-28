5cf6022e2e24e4575133f0fa2a1559790c66c799
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-28 21:23:14 +0500

    Добавление обработки ошибок сервера

--- js/backend.js
+++ js/backend.js
@@ -2,31 +2,6 @@
 
 (function () {
 
-  // Функция для загрузки объявлений с сервера
-  window.download = function (onLoad, onError) {
-    var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.open('GET', LOAD_URL);
-    xhr.send();
-  };
-
   // Функция для отправки данных на сервер
   window.save = function (data, onLoad, onError) {
     var SAVE_URL = 'https://js.dump.academy/keksobooking';
@@ -51,4 +26,50 @@
     xhr.open('POST', SAVE_URL);
     xhr.send(data);
   };
+
+  // Функция для загрузки объявлений с сервера
+  window.download = function (onLoad, onError) {
+    var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+
+        default:
+          error = 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', LOAD_URL);
+    xhr.send();
+  };
+
 })();
