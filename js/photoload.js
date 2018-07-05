f453830b5f6d4f40b7f003e10ed4caff6ac46df8
Elena <k-i-s-u-n-j-a@list.ru>
2018-07-05 21:13:42 +0500

    Добавлена возможность загрузки фото жилья

--- js/photoload.js
+++ js/photoload.js
@@ -4,8 +4,18 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var fileChooser = document.querySelector('.ad-form__input');
-  // var photoContainer = document.querySelector('.ad-form__photo-container');
-  // var preview = document.querySelector('.ad-form__photo');
+
+  var photoBlock = document.querySelector('.ad-form__photo-container');
+
+  var template = photoBlock.querySelector('.ad-form__photo--template');
+
+  var renderPreview = function (link) {
+    var previewElement = document.createElement('div');
+    previewElement.classList.add('ad-form__photo');
+    previewElement.innerHTML = '<img src="" width="44" height="44" alt="Фотография жилья пользователя">';
+    previewElement.querySelector('img').src = link;
+    return previewElement;
+  };
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
@@ -19,10 +29,8 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        // тут надо тег img добавлять
-        // проверить насчёт множественной загрузки
-        // + добавить сброс фото в reset
-        // preview.src = reader.result;
+        template.remove();
+        photoBlock.appendChild(renderPreview(reader.result));
       });
 
       reader.readAsDataURL(file);
